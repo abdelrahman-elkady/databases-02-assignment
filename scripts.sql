e4efd1c8db3ca1f5d5ffb3e85fa4aeb76604889f
kady <elkadyabdelrahman@gmail.com>
2015-05-03 17:03:45 +0200

    Updating scripts for part A and B

--- scripts.sql
+++ scripts.sql
@@ -1,5 +1,5 @@
 CREATE TABLE cup_matches (
-	mid serial PRIMARY KEY, 
+	mid serial PRIMARY KEY,
 	round varchar(20),
 	year integer ,
 	num_ratings integer ,
@@ -14,3 +14,20 @@ CREATE TABLE played_in(
 	PRIMARY KEY(mid,name)
 
 );
+
+-- A.1
+
+EXPLAIN SELECT * from played_in WHERE position=1;
+
+-- A.3
+
+CREATE INDEX position_index ON played_in(position);
+
+-- B.1
+
+SELECT * from played_in WHERE name like '%pele%';
+
+
+-- B.3
+
+CREATE INDEX name_index ON played_in(name);
