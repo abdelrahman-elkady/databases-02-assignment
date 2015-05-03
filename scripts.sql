0b2320ded6e18a5415571dd22dc234cf2e50570d
kady <elkadyabdelrahman@gmail.com>
2015-05-03 18:21:25 +0200

    Adding scripts for part C and D

--- scripts.sql
+++ scripts.sql
@@ -31,3 +31,34 @@ SELECT * from played_in WHERE name like '%pele%';
 -- B.3
 
 CREATE INDEX name_index ON played_in(name);
+
+-- B.5
+
+SET enable_seqscan = off; -- Y U NO USE MY INDEX ?
+EXPLAIN SELECT * from played_in WHERE name like '%pele%';
+
+-- C.1
+
+EXPLAIN SELECT * from cup_matches WHERE rating*3 > 20;
+
+-- C.3
+
+CREATE INDEX rating_index ON cup_matches(rating);
+
+-- D.1
+
+EXPLAIN SELECT * from cup_matches, played_in WHERE cup_matches.year=played_in.year;
+
+-- D.2
+
+CREATE INDEX year_index ON cup_matches(year) ;
+
+-- D.4
+
+CREATE INDEX year_index_played_in ON played_in(year);
+
+-- D.5
+
+SET enable_seqscan = OFF ;
+
+EXPLAIN SELECT * from cup_matches, played_in WHERE cup_matches.year=played_in.year;
